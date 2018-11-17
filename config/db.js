b8ec2712d6c76d6b78d3f7fa214bdd2ba2e8375f
matusekma <matusekma@gmail.com>
2018-11-17 21:24:41 +0100

    first commit

--- config/db.js
+++ config/db.js
@@ -1,4 +1,7 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/zfd4kx', { useNewUrlParser: true });
+var mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb://zfd4kxfelho:7fgfpmdamIfkHTgOuxzyb4glcXT37BjUok61WvEkc9eMq1ViTF8zfi40DgIqg30o7YGjFtvD6Qz82Xc1g4E9sQ==@zfd4kxfelho.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false",
+  { useNewUrlParser: true }
+);
 
 module.exports = mongoose;
---
+++ config/env/local-production.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db: {
+    uri:
+      "mongodb://zfd4kxfelho:7fgfpmdamIfkHTgOuxzyb4glcXT37BjUok61WvEkc9eMq1ViTF8zfi40DgIqg30o7YGjFtvD6Qz82Xc1g4E9sQ==@zfd4kxfelho.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false"
+  }
+};
