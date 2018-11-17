b8ce418013068cad33d94cb8c8c1b5922ce2674a
matusekma <matusekma@gmail.com>
2018-11-17 22:26:01 +0100

    deploy

--- models/actor.js
+++ models/actor.js
@@ -1,19 +1,18 @@
-var Schema = require('mongoose').Schema;
-var db = require('../config/db');
+var Schema = require("mongoose").Schema;
+var db = require("../config/db");
 
-
-var Actor = db.model('Actor', {
-    name: String,
-    birthYear: Number,
-    nationality: String,
-    comment: String,
-    description: String,
-    picName: String,
-    _user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    _films:  [{ type: Schema.Types.ObjectId, ref: 'Film' }]
+var Actor = db.model("Actor", {
+  name: String,
+  birthYear: Number,
+  nationality: String,
+  comment: String,
+  description: String,
+  picName: String,
+  _user: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  },
+  _films: [{ type: Schema.Types.ObjectId, ref: "Film" }]
 });
 
 module.exports = Actor;
