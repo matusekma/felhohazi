469715bf02d03e13d9639f2469c36381a51b1dcd
matusekma <matusekma@gmail.com>
2018-11-17 23:05:12 +0100

    deployment proba

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
-var express = require('express');
-var multer = require('multer');
+var express = require("express");
+var multer = require("multer");
 var app = express();
 
-var session = require('express-session');
-var bodyParser = require('body-parser');
+var session = require("express-session");
+var bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(session({
-    secret: 'keyboard cat',
+app.use(
+  session({
+    secret: "keyboard cat",
     cookie: {
-        maxAge: 60000
+      maxAge: 60000
     },
     resave: true,
     saveUninitialized: false
-}));
+  })
+);
 
 // for parsing application/json
 app.use(bodyParser.json());
 // for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 
-app.use(function (req, res, next) {
-    res.locals.error = [];
+app.use(function(req, res, next) {
+  res.locals.error = [];
 
-    return next();
+  return next();
 });
 
-require('./routes/main')(app);
+require("./routes/main")(app);
 
-app.use(function (err, req, res, next) {
-    res.status(500).send('Houston, we have a problem!');
+app.use(function(err, req, res, next) {
+  res.status(500).send("Houston, we have a problem!");
 
-    //Flush out the stack to the console
-    console.error(err.stack);
+  //Flush out the stack to the console
+  console.error(err.stack);
 });
 
-const server = app.listen(3000, function () {
-});
+const server = app.listen(process.env.PORT || 8443, function() {});
