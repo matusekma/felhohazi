f53f59d022d519a23cebffedc3b4a7581ae42d36
matusekma <matusekma@gmail.com>
2018-11-20 09:13:22 +0100

    connstr removed

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 var mongoose = require("mongoose");
 mongoose.connect(
-  //"mongodb://zfd4kxfelho:7fgfpmdamIfkHTgOuxzyb4glcXT37BjUok61WvEkc9eMq1ViTF8zfi40DgIqg30o7YGjFtvD6Qz82Xc1g4E9sQ==@zfd4kxfelho.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false",
   process.env.MONGODB_URI,
   //'mongodb://localhost/zfd4kxfelho',
   { useNewUrlParser: true }
